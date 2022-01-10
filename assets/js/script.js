b978072d32890b7691725d2c7428fbf1aa1bc26c
Jean McLeod <abricsltd@gmail.com>
2022-01-10 13:02:48 +0000

    end of tutor support

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const startbtn=document.getElementById("startbtn")
+
+startbtn.addEventListener("click",(event)=>{
+    event.preventDefault()
+    console.log("the button is clicking")
+})
+
+
+// create an array of objects with questions and answers for material to work with
+//create a counter function interval time function eg set interval etc look in activities
+// how to do css in js to make one page disapper and the other appear eg when you press start the questions appear
+//it can only show when the button is clicked to start
