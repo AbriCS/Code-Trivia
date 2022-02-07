3cd81aa95cff8316c1ebb6b0ffb274fee22b2b5f
Jean McLeod <abricsltd@gmail.com>
2022-02-07 12:54:03 +0000

    adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ const btn1El=document.getElementById("answer1");
 const btn2El=document.getElementById("answer2");
 const btn3El=document.getElementById("answer3");
 const btn4El=document.getElementById("answer4");
+const timercontainer=document.querySelector(".timer")
+const timerseconds=document.getElementById("timer_seconds")
+
 const currentQuestionIndex=0
 
 
@@ -49,6 +52,7 @@ startbtn.addEventListener("click",(event)=>{
     console.log("the button is clicking")
 startHeader.style.display="none"
 startQuiz()
+startTimer()
 })
 
 
@@ -60,19 +64,31 @@ btn2El.textContent=questions[currentQuestionIndex].choice2;
 btn3El.textContent=questions[currentQuestionIndex].choice3;
 btn4El.textContent=questions[currentQuestionIndex].choice4;
 }
+
+let interval;
+/* this sets the timer and -- starts a countdown*/
+
+let time=100
+function startTimer(){
+timercontainer.classList.remove("hide")
+    interval=setInterval(function() {
+time--
+timerseconds.innerHTML=time
+}, 1000)
+}
 /* Timer Section*/
 
-var countDownTime = new Time (1).getTime();
+/*var countDownTime = new Time (1).getTime();
 var x = setInterval(function(){})
 var now = new Date().getTime();
 var distance = countDownTime - now;
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 document.getElementById("timed").innerHTML = days + "seconds + "s";
 
-/* Time up section*/
+Time up section
 const myTimeout = setTimeout (myGreeting, 5000);
 function myGreeting() { document.getElementById("Timeup").innerHTML= "Your Time is Up!"
-if(distance <0) {clearInterval(x);}
+if(distance <0) {clearInterval(x);}*/
 
 
 //create a counter function interval time function eg set interval etc look in activities
