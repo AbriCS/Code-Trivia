7bb261a40593dcf87868655e15ffdfb6683ff16b
Jean McLeod <abricsltd@gmail.com>
2022-03-08 22:05:10 +0000

    update questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,11 @@ const btn1El=document.getElementById("answer1");
 const btn2El=document.getElementById("answer2");
 const btn3El=document.getElementById("answer3");
 const btn4El=document.getElementById("answer4");
-const timercontainer=document.querySelector(".timer")
-const timerseconds=document.getElementById("timer_seconds")
-
-const currentQuestionIndex=0
+const container2=document.getElementById("container2");
+const timercontainer=document.querySelector(".timer");
+const timerseconds=document.getElementById("timer_seconds");
+const answer=document.getElementById("ans-btn");
+let currentQuestionIndex=0
 
 
 
@@ -36,15 +37,24 @@ const questions = [{
 },
 
 {
-    question: " which one is a Tag? ",
-    choice1: " footer",
-    choice2: " a C++ program",
-    choice3: " a letter in the alphabet",
-    choice4: " an element on the periodic table",
-    correct: " footer"
+    question: " Which answer is a Tag? ",
+    choice1: " Footer",
+    choice2: " Header",
+    choice3: " Body",
+    choice4: " All three",
+    correct: " All three"
 
 }
 
+{
+    question: " What is HTML short for? ",
+    choice1: " Hypertext Markup Language",
+    choice2: " High Tree Markup Language",
+    choice3: " Huge Tree Markup Language",
+    choice4: " AHigh Text Markup Language",
+    correct: " Hypertext Markup Language"
+
+}
 ]
 
 startbtn.addEventListener("click",(event)=>{
@@ -57,12 +67,21 @@ startTimer()
 
 
 function startQuiz(){
-questionContainer.style.display="block"
+    if (currentQuestionIndex===questions.length){
+     endQuiz()   
+    }
+questionContainer.classList.replace("hide","show")
 questionEL.textContent=questions[currentQuestionIndex].question   
 btn1El.textContent=questions[currentQuestionIndex].choice1; 
 btn2El.textContent=questions[currentQuestionIndex].choice2;
 btn3El.textContent=questions[currentQuestionIndex].choice3;
 btn4El.textContent=questions[currentQuestionIndex].choice4;
+
+//add the value attribute to buttons with the answers to Qs
+btn1El.setAttribute("value", questions[currentQuestionIndex].choice1 )
+btn2El.setAttribute("value", questions[currentQuestionIndex].choice2 )
+btn3El.setAttribute("value", questions[currentQuestionIndex].choice3 )
+btn4El.setAttribute("value", questions[currentQuestionIndex].choice4 )
 }
 
 let interval;
@@ -76,6 +95,37 @@ time--
 timerseconds.innerHTML=time
 }, 1000)
 }
+
+let score=0
+
+function checkAnswer(userChoice){
+if (userChoice===questions[currentQuestionIndex].correct){
+    currentQuestionIndex++ 
+    startQuiz()
+    score++
+} else {
+    currentQuestionIndex++
+    time=time -10
+    startQuiz()
+}
+
+}
+let finalScore
+
+function endQuiz(){
+    clearInterval(interval)
+    questionContainer.style.display="none"
+finalScore=finalScore
+container2.classList.remove("hide")
+}
+//add event listener to target the button with the answers
+answer.addEventListener("click",(event)=>{
+    event.preventDefault()
+    let btnclick = this.event.target.value
+    console.log (btnclick)
+    checkAnswer(btnclick)
+})
+
 /* Timer Section*/
 
 /*var countDownTime = new Time (1).getTime();
